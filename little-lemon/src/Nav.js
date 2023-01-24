7999ce89e6f6eb4b225cccca8560154b2d54cb6a
unknown <abu.eyaaaad@gmail.com>
2023-01-24 05:44:38 +0200

    added responsiveness

--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Logo from './assets/Logo.svg'
 
 const Nav = () => {
+  const navigate = useNavigate()
   return (
     <nav>
-        <a role="button" href="/">
-            <img
-                src={Logo}
-                alt="logo"
-            />
-        </a>
+        <img
+            src={Logo}
+            alt="logo"
+            onClick={() => navigate('/')}
+        />
         <ul className="main-navigation">
             <li><a href="/" target="_blank">Home</a></li>
             <li><a href="/about" target="_blank">About</a></li>
